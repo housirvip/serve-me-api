84fd6690174e98cacc620a3e031851a35830d26e
housirvip <housirvip@outlook.com>
2020-04-03 21:56:38 -0500

    update ecosystem.config.js package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps : [{
     name: 'strapi',
     script: 'npm',
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'start',
     instances: 1,
+    version: '0.1.5',
     error_file: 'err.log',
     out_file: 'out.log',
     log_file: 'combined.log',
     time: true,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '500M',
     env: {
       NODE_ENV: 'development',
       GOOGLE_APPLICATION_CREDENTIALS: '/Users/housirvip/Documents/serve-me-ionic-firebase-adminsdk-44oms-33c98c32b3.json',
@@ -28,15 +28,5 @@ module.exports = {
       DB_USER: 'root',
       DB_PASSWORD: 'housirvip'
     }
-  }],
-  deploy : {
-    production : {
-      user : 'user',
-      host : 'host',
-      ref  : 'origin/master',
-      repo : 'git@github.com:housirvip/serve-me-api.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
